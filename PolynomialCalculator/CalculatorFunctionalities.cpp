8af645c93231bd95f84a6b49d3a7a5a3ca1d12a9
Kristian2705 <k_petrov27@abv.bg>
2025-01-13 18:31:43 +0200

    added identity comment in CalculatorFunctionalities

--- PolynomialCalculator/CalculatorFunctionalities.cpp
+++ PolynomialCalculator/CalculatorFunctionalities.cpp
@@ -1,3 +1,16 @@
+/**
+*
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2024/2025
+*
+* @author Kristian Petrov
+* @idnumber 3MI0600416
+* @compiler VCC
+*
+*/
+
 #include "CalculatorFunctionalities.h"
 #include "HelperFunctions.h"
 #include "Constants.h"
