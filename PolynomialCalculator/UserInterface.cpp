46fa465e28349ac15ec882d19111e5bcbdfefc87
Kristian2705 <k_petrov27@abv.bg>
2025-01-19 20:24:18 +0200

    fixed getOption validation and extracted the text into a function

--- PolynomialCalculator/UserInterface.cpp
+++ PolynomialCalculator/UserInterface.cpp
@@ -19,25 +19,14 @@ void polynomialCalculator() {
 
 	bool exitCommand = false;
 
-	std::cout << 
-		"Welcome to Polynomial Calculator - a mini project intended to work with polynomials with rational coefficients" 
-		<< std::endl;
-	std::cout << "Choose one of the following functionalities:" << std::endl;
-	std::cout << "1) Add polynomials" << std::endl;
-	std::cout << "2) Subtract polynomials" << std::endl;
-	std::cout << "3) Multiply polynomials" << std::endl;
-	std::cout << "4) Divide polynomials" << std::endl;
-	std::cout << "5) Multiply polynomial by scalar" << std::endl;
-	std::cout << "6) Find value of polynomial at a given number" << std::endl;
-	std::cout << "7) Find GCD of two polynomials" << std::endl;
-	std::cout << "8) Display Vieta's formulas for a given polynomial" << std::endl;
-	std::cout << "9) Represent a polynomial in powers of (x+a)" << std::endl;
-	std::cout << "10) Factor polynomial and find its rational roots" << std::endl;
-	std::cout << "11) Quit program" << std::endl;
-	std::cout << std::endl;
+	printAllFunctionalities();
 
 	do {
 		int menuOption = getOption();
+		while (!isValidOption(menuOption)) {
+			std::cout << "Invalid command! Please enter an option from 1 to 11!" << std::endl;
+			menuOption = getOption();
+		}
 		std::cout << std::endl;
 		switch (menuOption)
 		{
@@ -79,4 +68,24 @@ void polynomialCalculator() {
 				break;
 		}
 	} while (!exitCommand);
+}
+
+void printAllFunctionalities()
+{
+	std::cout <<
+		"Welcome to Polynomial Calculator - a mini project intended to work with polynomials with rational coefficients"
+		<< std::endl;
+	std::cout << "Choose one of the following functionalities:" << std::endl;
+	std::cout << "1) Add polynomials" << std::endl;
+	std::cout << "2) Subtract polynomials" << std::endl;
+	std::cout << "3) Multiply polynomials" << std::endl;
+	std::cout << "4) Divide polynomials" << std::endl;
+	std::cout << "5) Multiply polynomial by scalar" << std::endl;
+	std::cout << "6) Find value of polynomial at a given number" << std::endl;
+	std::cout << "7) Find GCD of two polynomials" << std::endl;
+	std::cout << "8) Display Vieta's formulas for a given polynomial" << std::endl;
+	std::cout << "9) Represent a polynomial in powers of (x+a)" << std::endl;
+	std::cout << "10) Factor polynomial and find its rational roots" << std::endl;
+	std::cout << "11) Quit program" << std::endl;
+	std::cout << std::endl;
 }
--- PolynomialCalculator/UserInterface.h
+++ PolynomialCalculator/UserInterface.h
@@ -3,3 +3,5 @@
 #include <iostream>
 
 void polynomialCalculator();
+
+void printAllFunctionalities();
